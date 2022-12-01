1f87c970ab34c6a75ae3872cb5d9b711f8f26f8e
Rowine <rowine12345@gmail.com>
2022-12-01 08:48:36 +0800

    Connect to a database

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -29,12 +29,12 @@ const Category = () => {
 
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
             {categoryProducts.map((product: IProductItem) => (
-              <div key={product.id} className='group relative'>
+              <div key={product._id} className='group relative'>
                 <div className='min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none'>
-                  <Link to={`/${product.category}/${product.id}`}>
+                  <Link to={`/${product.category}/${product._id}`}>
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={product.image}
+                      alt={product.name}
                       className='h-full w-full object-cover object-center lg:h-full lg:w-full'
                     />
                   </Link>
@@ -43,7 +43,7 @@ const Category = () => {
                   <div>
                     <h3 className='text-sm text-gray-700'>
                       <Link
-                        to={`/${product.category}/${product.id}`}
+                        to={`/${product.category}/${product._id}`}
                         className='font-semibold hover:underline'
                       >
                         <span aria-hidden='true' className='absolute inset-0' />
